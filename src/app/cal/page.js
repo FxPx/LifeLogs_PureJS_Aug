4085d8c2e4222d7c775ad7c46be6765f1c14a624
id8r <99732569+FxPx@users.noreply.github.com>
2024-08-13 00:06:17 +0530

    JLT

--- src/app/cal/page.js
+++ src/app/cal/page.js
@@ -1,34 +1,3 @@
-// /* - - - - - - - - - - - - - - - - - - - - */
-// /* src/app/cal/page.js | Calendar Page | Sree | 12 Aug 2024 */
-// /* - - - - - - - - - - - - - - - - - - - - */
-
-// "use client";
-
-// import React, { useState } from 'react';
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css'; // Import the default styles for react-calendar
-// import '../cal/cal.css'
-
-// const CalendarPage = () => {
-//     const [date, setDate] = useState(new Date());
-
-//     return (
-//         <>
-//             <Calendar
-//                 onChange={setDate}
-//                 value={date}
-//                 className="styleCal"
-//             />
-//         </>
-//     );
-// };
-
-// export default CalendarPage;
-// /* - - - - - - - - - - - - - - - - - - - - */
-
-
-
-
 /* - - - - - - - - - - - - - - - - - - - - */
 /* src/app/cal/page.js | Calendar Page | Sree | 12 Aug 2024 */
 /* - - - - - - - - - - - - - - - - - - - - */
@@ -39,13 +8,13 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../cal/cal.css';
 
-const CalendarPage = () => {
+const fxCalendar = () => {
     const today = new Date();
     const year = today.getFullYear();
     const months = Array.from({ length: 12 }, (_, i) => new Date(year, i, 1));
 
     // Check if a date is the same as today
-    const isSameDay = (date) => 
+    const isSameDay = (date) =>
         date.getDate() === today.getDate() &&
         date.getMonth() === today.getMonth() &&
         date.getFullYear() === today.getFullYear();
@@ -56,7 +25,7 @@ const CalendarPage = () => {
             <div className="calendarGrid">
                 {months.map((monthDate, index) => (
                     <div key={index} className="monthCalendar">
-                        <h2>{monthDate.toLocaleString('default', { month: 'long' })}</h2>
+                        <h4>{monthDate.toLocaleString('default', { month: 'long' })}</h4>
                         <Calendar
                             defaultActiveStartDate={monthDate}
                             tileClassName={({ date, view }) =>
@@ -73,5 +42,5 @@ const CalendarPage = () => {
     );
 };
 
-export default CalendarPage;
+export default fxCalendar;
 /* - - - - - - - - - - - - - - - - - - - - */
